e4232d77cc53bd426892bca36ec22e7195414755
kh4l1d <khalidahmedsayed@gmail.com>
2017-04-03 22:03:19 +0530

    Murtaza's reloaded and thread bug fixed clean code

--- OddEvenSort/concurrentOddEven.py
+++ OddEvenSort/concurrentOddEven.py
@@ -1,29 +1,25 @@
 #Author: Murtaza - Bass naam hi kafi hai
 from threading import Thread
 
-def even(x):
-	for i in range(0, len(x)-1, 2):
+def sort(x,l):
+	for i in range(l, len(x)-1, 2):
 			if x[i] > x[i+1]:
 				x[i], x[i+1] = x[i+1], x[i]
 				sorted = False
-def odd(x):
-	for i in range(1, len(x)-1, 2):
-			if x[i] > x[i+1]:
-				x[i], x[i+1] = x[i+1], x[i]
-				sorted = False
-
+			if x[i] == x[i+1]:
+				i+=2
 def oddevensort(x):
 	sorted = False
 	while not sorted:
 		sorted = True
-		t =Thread(target = even(x))
-		t1 =Thread(target = odd(x))
+		t =Thread(target = sort(x,0)) # for even
+		t1 =Thread(target = sort(x,1)) # for odd
 		t.start()
 		t1.start()
 		t.join()
 		t1.join()
 	for i in range(0,len(x)-1):
-		if x[i]<x[i+1]:
+		if x[i]<=x[i+1]:
 			sorted = False
 		else:
 			x=oddevensort(x)
