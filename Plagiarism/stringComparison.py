505ca68b1216bf66d66d11b8f88a47826e904a80
kh4l1d <khalidahmedsayed@gmail.com>
2017-04-20 13:21:19 +0530

    Cleaned up variable names

--- Plagiarism/stringComparison.py
+++ Plagiarism/stringComparison.py
@@ -1,13 +1,13 @@
 import string # string module is shipped with python by default
 
 def apparentlyMaturePlagiarismChecker(text1 , text2) :
-    sanskariCharacters = string.letters + string.digits + "- "   # removing all punctuations and stuff other than - and space and of course letters and alphabets
+    cleanCharacters = string.letters + string.digits + "- "   # removing all punctuations and stuff other than - and space and of course letters and alphabets
 
-    sanskariString1 = filter(lambda c: c in sanskariCharacters, text1)
-    sanskariString2 = filter(lambda c: c in sanskariCharacters, text2)
+    cleanString1 = filter(lambda c: c in cleanCharacters, text1)
+    cleanString2 = filter(lambda c: c in cleanCharacters, text2)
 
-    bagOfSupposedlyPureWords1 = sanskariString1.split() # split() removes all kinds of white space (spaces,tabs,newlines and what not)
-    bagOfSupposedlyPureWords2 = sanskariString2.split()
+    bagOfSupposedlycleanWords1 = cleanString1.split() # split() removes all kinds of white space (spaces,tabs,newlines and what not)
+    bagOfSupposedlycleanWords2 = cleanString2.split()
 
 #====================================================
 # OPTIONAL - remove these common words so that Plagiarism won't be detected because of high content of these common words
@@ -16,27 +16,27 @@ def apparentlyMaturePlagiarismChecker(text1 , text2) :
 # "Apsara is a good eraser"
 # "Apsara is not a rubber"
 # will show plagiarism (because 'is' and 'a' are matching) although the statements are very very different indeed
-    if "is" in bagOfSupposedlyPureWords1 : bagOfSupposedlyPureWords1.remove("is")
-    if "the" in bagOfSupposedlyPureWords1 : bagOfSupposedlyPureWords1.remove("the")
-    if "a" in bagOfSupposedlyPureWords1 : bagOfSupposedlyPureWords1.remove("a")
-    if "in" in bagOfSupposedlyPureWords1 : bagOfSupposedlyPureWords1.remove("in")
+    if "is" in bagOfSupposedlycleanWords1 : bagOfSupposedlycleanWords1.remove("is")
+    if "the" in bagOfSupposedlycleanWords1 : bagOfSupposedlycleanWords1.remove("the")
+    if "a" in bagOfSupposedlycleanWords1 : bagOfSupposedlycleanWords1.remove("a")
+    if "in" in bagOfSupposedlycleanWords1 : bagOfSupposedlycleanWords1.remove("in")
 
-    if "is" in bagOfSupposedlyPureWords2 : bagOfSupposedlyPureWords2.remove("is")   # bet you didn't know 'if' could be written like this
-    if "the" in bagOfSupposedlyPureWords2 : bagOfSupposedlyPureWords2.remove("the")
-    if "a" in bagOfSupposedlyPureWords2 : bagOfSupposedlyPureWords2.remove("a")
-    if "in" in bagOfSupposedlyPureWords2 : bagOfSupposedlyPureWords2.remove("in")
+    if "is" in bagOfSupposedlycleanWords2 : bagOfSupposedlycleanWords2.remove("is")   # bet you didn't know 'if' could be written like this
+    if "the" in bagOfSupposedlycleanWords2 : bagOfSupposedlycleanWords2.remove("the")
+    if "a" in bagOfSupposedlycleanWords2 : bagOfSupposedlycleanWords2.remove("a")
+    if "in" in bagOfSupposedlycleanWords2 : bagOfSupposedlycleanWords2.remove("in")
 # i've just added a few word, you can add more
 #======================================================
     # so essentially we'll be comparing how many words in these 2 lists will match
     # this is called bag of words checking
-    kitneWordsCopyKiye = len(set(bagOfSupposedlyPureWords1) & set(bagOfSupposedlyPureWords2)) # this gets us the length of the set that is the intersection of the two sets - effectively giving us the count of common words
+    kitneWordsCopyKiye = len(set(bagOfSupposedlycleanWords1) & set(bagOfSupposedlycleanWords2)) # this gets us the length of the set that is the intersection of the two sets - effectively giving us the count of common words
     similarityFlag = False
     smallerLength = 0
 
-    if len(bagOfSupposedlyPureWords1) <= len(bagOfSupposedlyPureWords2) :
-        smallerLength = len(bagOfSupposedlyPureWords1)
+    if len(bagOfSupposedlycleanWords1) <= len(bagOfSupposedlycleanWords2) :
+        smallerLength = len(bagOfSupposedlycleanWords1)
     else :
-        smallerLength = len(bagOfSupposedlyPureWords2)
+        smallerLength = len(bagOfSupposedlycleanWords2)
 
     if kitneWordsCopyKiye >= int((0.5 * smallerLength)) :
         similarityFlag = True
